7fd2823f177452221840cfbb9d683850f18272dc
wangyifani <617498836@qq.com>
2022-02-18 11:38:29 +0800

    Update main.go

---
+++ README.md
@@ -0,0 +1,106 @@
+# JsRPC
+
+-- js逆向之远程调用(rpc)免去抠代码补环境
+
+> tip:懒得自己编译的 ，[releases](https://github.com/jxhczhl/JsRpc/releases)中有已经编译好的包 （win、Linux、MacOS的都有~）
+
+## 目录结构
+
+```dart
+-- main.go (服务器的主代码)
+-- resouces/JsEnv.js (客户端注入js环境)
+```
+
+## 基本介绍
+
+运行服务器程序和js脚本 即可让它们通信，实现调用接口执行js获取想要的值(加解密)
+
+## 实现
+
+原理：在网站的控制台新建一个WebScoket客户端链接到服务器通信，调用服务器的接口 服务器会发送信息给客户端 客户端接收到要执行的方法执行完js代码后把获得想要的内容发回给服务器 服务器接收到后再显示出来
+
+> 说明：本方法可以https证书且支持wss
+
+在https的网站想要新建WebSocket连接如果是连接到普通的ws可能会报安全错误，好像连接本地(127.0.0.1)不会报错~ 可以用本地和wss 你自己看着玩
+
+1. 无https证书者。直接编译main.go 我试了一下，发现使用本地ip(127.0.0.1)可以在https的网站直接连接ws使用 默认端口12080
+2. 有https证书者。修改main.go文件 把r.Run()注释掉，把r.RunTls注释取消掉 并且参数设置证书的路径 直接输入名字就是当前路径 默认端口：12443
+
+> 另外的题外话，有域名没证书不会搞的 或者有域名有公网(非固定IP的)都可以搞成的，自己研究研究
+
+## 食用方法
+
+### 打开编译好的文件，开启服务
+
+如下图所示
+
+![image.png](./assets/image.png)
+
+**api 简介**
+
+- `/list` :查看当前连接的ws服务
+- `/ws`  :浏览器注入ws连接的接口
+- `/result` :获取数据的接口  (数据格式`json: {"group":"hhh","hello":"好困啊yes","name":"baidu","status":"200"}` )
+
+说明：接口用`group`和`name`来区分 如 `ws://127.0.0.1:12080/ws?group={}&name={}` //注入`ws`的例子 `group`和`name`都可以随便
+
+
+
+#### 示例接口
+
+`http://127.0.0.1:12080/go?group=hhh&name=baidu&action=hello&param=yes`
+
+参数解析说明
+
+`http://127.0.0.1:12080/go?group={}&name={}&action={}&param={}`    
+
+这是调用的接口 `group`和`name`填写上面注入时候的，`action`是注册的方法名,`param`是可选的参数（这个参数完全取决于`console`注入的方法）  
+
+
+### 注入JS，构建通信环境
+
+打开JsEnv 复制粘贴到网站控制台(注意有时要放开断点)
+
+![image](https://user-images.githubusercontent.com/41224971/134774597-5c8c845b-072e-40d1-bdf7-24e89f78b22e.png)
+
+### 注入ws与方法
+
+```js
+// 连接通信
+var demo = new Hlclient("ws://127.0.0.1:12080/ws?group=hhh&name=baidu");
+// 注册一个方法 第一个参数hello为方法名，
+// 第二个参数为函数，resolve里面的值是想要的值(发送到服务器的)
+// param是可传参参数，可以忽略
+demo.regAction("hello", function (resolve, param) {
+    var c = "好困啊" + param;
+    resolve(c);
+})
+```
+
+![image](https://user-images.githubusercontent.com/41224971/134774859-a4594f23-b828-4538-8b89-9d96813f7d1e.png)
+
+### 访问接口，获得数据
+
+```dart
+http://127.0.0.1:12080/go?group=hhh&name=baidu&action=hello&param=yes
+// 其中 hello是会变的 是action名字。 用代码访问的时候要注意这个名字
+{
+  "group":"hhh",
+  "hello":"好困啊yes",
+  "name":"baidu",
+  "status":"200"
+}
+```
+
+![image](https://user-images.githubusercontent.com/41224971/134775037-167724d4-ae94-4fcf-88c4-d881621b712c.png)
+
+## 食用方法
+
+### 控制台直接注入方法
+
+1. 将`resouces/JsEnv.js`代码完全复制，在浏览器`f12`控制台直接粘贴回车即可
+2. 将上方的**注入ws与方法** 同样的复制然后在控制台粘贴回车
+
+### 油猴脚本
+
+推荐使用油猴脚本在浏览器外部挂载js,示例代码：`https://greasyfork.org/zh-CN/scripts/440234-%E6%8A%96%E9%9F%B3json-rpc`
---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/unrolled/secure"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+var (
+	// BasicPort The original port without SSL certificate
+	BasicPort = `:12080`
+	// SSLPort "Secure" port with SSL certificate
+	SSLPort = `:12443`
+	// websocket.Upgrader specifies parameters for upgrading an HTTP connection to a
+	// WebSocket connection.
+	upGrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+	hlSyncMap sync.Map
+
+	// OverTime 设置接口没得到结果时的超时时间
+	OverTime = time.Second * 30
+)
+
+type Clients struct {
+	clientGroup string
+	clientName  string
+	Data        map[string]string
+	clientWs    *websocket.Conn
+}
+
+// NewClients initializes a new Clients instance
+func NewClient(group string, name string, ws *websocket.Conn) *Clients {
+	return &Clients{
+		clientGroup: group,
+		clientName:  name,
+		Data:        make(map[string]string),
+		clientWs:    ws,
+	}
+}
+
+// ws, provides inject function for a job
+func ws(c *gin.Context) {
+	group, name := c.Query("group"), c.Query("name")
+	if group == "" || name == "" {
+		return
+	}
+	ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		fmt.Println("websocket err:", err)
+		return
+	}
+	client := NewClient(group, name, ws)
+	hlSyncMap.Store(group+"->"+name, client)
+	for {
+		//等待数据
+		_, message, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		msg := string(message)
+		check := []uint8{104, 108, 94, 95, 94}
+		strIndex := strings.Index(msg, string(check))
+		if strIndex >= 1 {
+			action := msg[:strIndex]
+			client.Data[action] = msg[strIndex+5:]
+			fmt.Println("get_message:", client.Data[action])
+			hlSyncMap.Store(group+"->"+name, client)
+		} else {
+			fmt.Println(msg, "message error")
+		}
+
+	}
+	defer func(ws *websocket.Conn) {
+		_ = ws.Close()
+	}(ws)
+
+}
+
+func QueryFunc(client *Clients, funcName string, param string) {
+	var WriteDate string
+	if param == "" {
+		WriteDate = "{\"action\":\"" + funcName + "\"}"
+	} else {
+		WriteDate = "{\"action\":\"" + funcName + "\",\"param\":\"" + param + "\"}"
+	}
+	ws := client.clientWs
+	err := ws.WriteMessage(1, []byte(WriteDate))
+	if err != nil {
+		fmt.Println(err, "写入数据失败")
+	}
+
+}
+
+func ResultSet(c *gin.Context) {
+	getGroup, getName, Action, getParam := c.Query("group"), c.Query("name"), c.Query("action"), c.Query("param")
+	if getGroup == "" || getName == "" {
+		c.String(200, "input group and name")
+		return
+	}
+	clientName, ok := hlSyncMap.Load(getGroup + "->" + getName)
+	if ok == false {
+		c.String(200, "注入了ws？没有找到当前组和名字")
+		return
+	}
+	if Action == "" {
+		c.JSON(200, gin.H{"group": getGroup, "name": getName})
+		return
+	}
+	//取一个ws客户端
+	client, ko := clientName.(*Clients)
+	if !ko {
+		return
+	}
+	//发送数据到web里得到结果
+	QueryFunc(client, Action, getParam)
+
+	ctx, cancel := context.WithTimeout(context.Background(), OverTime)
+	for {
+		select {
+		case <-ctx.Done():
+			// 获取数据超时了
+			cancel()
+			return
+		default:
+			data := client.Data[Action]
+			//fmt.Println("正常中")
+			if data != "" {
+				cancel()
+				//这里设置为空是为了清除上次的结果并且赋值判断
+				client.Data[Action] = ""
+				c.JSON(200, gin.H{"status": "200", "group": client.clientGroup, "name": client.clientName, Action: data})
+			} else {
+				time.Sleep(time.Millisecond * 500)
+			}
+		}
+	}
+
+}
+
+func getList(c *gin.Context) {
+	resList := "hliang:\r\n"
+	hlSyncMap.Range(func(key, value interface{}) bool {
+		resList += key.(string) + "\r\n\t"
+
+		return true
+	})
+	c.String(200, resList)
+}
+
+func Index(c *gin.Context) {
+	c.String(200, "你好，我是黑脸怪~")
+}
+
+func TlsHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		secureMiddleware := secure.New(secure.Options{
+			SSLRedirect: true,
+			SSLHost:     SSLPort,
+		})
+		err := secureMiddleware.Process(c.Writer, c.Request)
+		if err != nil {
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", Index)
+	r.GET("/go", ResultSet)
+	r.GET("/ws", ws)
+	r.GET("/list", getList)
+	_ = r.Run(BasicPort)
+
+	//编译https版放开下面两行注释代码 并且把上一行注释
+	//r.Use(TlsHandler())
+	//_ = r.RunTLS(SSLPort, "zhengshu.pem", "zhengshu.key")
+
+}
